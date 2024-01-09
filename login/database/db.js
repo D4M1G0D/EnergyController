52d463a61cd1a8f351991d33e8cfde767f68360e
deyvidpaucar <deyvidp_n@hotmail.com>
2024-01-09 10:05:09 -0500

    login

--- login/database/db.js
+++ login/database/db.js
@@ -5,7 +5,9 @@ const connection = mysql.createConnection({
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
     password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE
+    database : process.env.DB_DATABASE,
+    port: process.env.DB_PORT || 3306, // Puerto predeterminado de MySQL
+    //tls:process.env.DB_TLS
 });
 
 connection.connect((error)=>{
